d558ea5eda9421cdf8a2c8590027ffe12bc63bf4
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2024-12-25 07:33:59 +0530

    Add files via upload

---
+++ week1/moveAll0.cpp
@@ -0,0 +1,83 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pll pair<long long, long long>
+#define pvi pair<vector<int>, int>
+#define yes          \
+    cout << "YES\n"; \
+    return;
+ 
+#define no          \
+    cout << "NO\n"; \
+    return;
+ 
+#define endl '\n'
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+ 
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+ 
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+ 
+#define get(n) \
+    int n;     \
+    cin >> n
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+ 
+void solve()
+{
+    get(n);
+    vector<int> nums(n);
+    read(nums);
+    int i;
+        int j = -1;
+        for(int  i = 0 ; i<n ;i++)
+           if(nums[i]==0) 
+           {
+               j  = i;
+               break;
+           }
+        //you moved to first zero
+        //no zero case
+        if(j==-1) return ;
+        for(int i = j+1; i<n ;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j = j+1;
+            }
+        }
+       print(nums);
+       return;
+}
+ 
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+        solve();
+    return 0;
+}
