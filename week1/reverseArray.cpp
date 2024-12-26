aa7483db2b6c7cd95a91c051c1c4261751e53cf7
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2024-12-26 06:29:13 +0530

    Add files via upload

---
+++ week1/reverseArray.cpp
@@ -0,0 +1,76 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pll pair<long long, long long>
+#define pvi pair<vector<int>, int>
+
+ 
+#define yes          \
+    cout << "YES\n"; \
+    return;
+ 
+#define no          \
+    cout << "NO\n"; \
+    return;
+ 
+#define endl '\n'
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+ 
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+ 
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+ 
+#define get(n) \
+    int n;     \
+    cin >> n
+ 
+const int N1 = 1e5 + 1;
+const int N2 = 1e7 + 1;
+const int MOD = 1e9 + 7;
+ 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+void reverseArray(vector<int> &arr) {
+        int n = arr.size();
+        if(n==0||n==1) return ;
+        for(int i =0  ; i<n/2 ; i++)
+        {
+            swap(arr[i],arr[n-i-1]);
+        }
+        print(arr);
+        return;
+    }
+/*template <typename T>
+void reverse(T& container) 
+{
+    std::reverse(container.begin(), container.end());
+}*/
+int32_t main()
+{
+    get(t);
+    while(t--)
+    {
+        get(n);
+        vector<int> arr(n);
+        read(arr);
+        reverseArray(arr);
+    }
+    return 0;
+}
