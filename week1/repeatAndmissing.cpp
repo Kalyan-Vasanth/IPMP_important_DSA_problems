a2922dbe10c8fe0fd2f205346cb32a02aa968601
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2024-12-27 19:17:36 +0530

    Add files via upload

---
+++ week1/repeatAndmissing.cpp
@@ -0,0 +1,97 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pll pair<long long, long long>
+#define pvi pair<vector<int>, int>
+
+ 
+#define yes          \
+    cout << "YES\n"; \
+    return;
+ 
+#define no          \
+    cout << "NO\n"; \
+    return;
+ 
+#define endl '\n'
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+ 
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+ 
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+ 
+#define get(n) \
+    int n;     \
+    cin >> n
+ 
+const int N1 = 1e5 + 1;
+const int N2 = 1e7 + 1;
+const int MOD = 1e9 + 7;
+ 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+int mod_exp(int base, int exp, int mod = MOD)
+{
+    int res = 1;
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+            res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
+void solve()
+{
+    get(n);
+    vector<int> arr(n);
+    read(arr);
+    int a,b;
+    for(int i = 0 ; i<n; i++)
+    {
+        //use the concept used  for missing  positive number
+        int ind = abs(arr[i])-1;
+        if(arr[ind]<0) a = ind+1; //if it is visiting second time 
+        else
+        {
+            arr[ind] = arr[ind]*-1;
+        }
+    }
+    for(int i = 0 ; i<n ; i++)
+    {
+        if(arr[i]>0){ b = i+1; break;}
+    }
+    cout<<a<<endl;
+    cout<<b<<endl;
+} 
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
