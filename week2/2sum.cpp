848933778155307c7ea9b6e832e9427f5ae5692a
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2025-01-06 08:51:35 +0530

    Add files via upload

---
+++ week2/2sum.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define yes          \
+    cout << "YES\n"; \
+    return; 
+#define no          \
+    cout << "NO\n"; \
+    return;
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+#define get(n) \
+    int n;     \
+    cin >> n
+const int N1 = 1e5 + 1;
+const int N2 = 1e7 + 1;
+const int MOD = 1e9 + 7; 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+void twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map<int,int> myMap;
+        vector<int> v;
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(myMap.find(target-nums[i])==myMap.end())
+            {
+                myMap[nums[i]] = i;
+            }
+            else
+            {
+                v.push_back(i);
+                v.push_back(myMap[target-nums[i]]);
+                break;
+            }
+        }
+        print(v); 
+    }
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+    {
+        get(n);
+        vi arr(n);
+        read(arr);
+        get(target);
+        twoSum(arr,target);
+    }
+    return 0;
+}
