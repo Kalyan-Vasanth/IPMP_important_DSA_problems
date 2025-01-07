65a9f1e350c49d10a1b88507b4bd3a79afd2d8fd
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2025-01-07 06:27:18 +0530

    Add files via upload

---
+++ week2/stock1.cpp
@@ -0,0 +1,68 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define yes          \
+    cout << "YES\n"; \
+    return; 
+#define no          \
+    cout << "NO\n"; \
+    return;
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+#define get(n) \
+    int n;     \
+    cin >> n
+const int N1 = 1e5 + 1;
+const int N2 = 1e7 + 1;
+const int MOD = 1e9 + 7; 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+int maxProfit(vector<int>& a) {
+        int n = a.size();
+        int maxp = 0;
+        int i = 0;
+        if(n==1) return maxp;
+        for(int j = 1 ; j<n ; j++)
+        {
+            if(a[j]<a[i]) i = j;
+            else if(a[j]>a[i])
+            {
+                int temp = a[j]-a[i];
+                maxp = max(maxp,temp);
+            }
+        }
+        return maxp;      
+    }
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+    {
+        get(n);
+        vi arr(n);
+        read(arr);  
+        int ans = maxProfit(arr);
+        prt(ans);
+    }
+    return 0;
+}
