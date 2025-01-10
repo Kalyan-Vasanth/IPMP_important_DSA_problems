ea110f4a4890407a35a071f5c3638137bccde0e8
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2025-01-10 07:35:41 +0530

    Add files via upload

---
+++ week2/subArrayProductK.cpp
@@ -0,0 +1,64 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define ll long long
+#define vvi vector<vector<int>>
+#define yes          \
+    cout << "YES\n"; \
+    return; 
+#define no          \
+    cout << "NO\n"; \
+    return;
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+#define get(n) \
+    int n;     \
+    cin >> n
+#define prt(x) cout << x << '\n'
+#define pb push_back
+const int MOD = 1e9 + 7; 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0 , r = 0 ;
+        int ans = 0 , prod = 1;
+        while(r<n)
+        {
+            prod = prod*nums[r];
+            while(prod>=k && l<=r)
+            {
+                prod = prod/nums[l];
+                l++;
+            }
+            ans = ans+r-l+1;
+            r++;
+        }
+        return ans;
+    }
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+    {
+        get(n);
+        vi arr(n);
+        read(arr);
+        get(target);
+        cout<<numSubarrayProductLessThanK(arr,target)<<endl;
+     }
+     return 0;
+}
