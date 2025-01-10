68f43842c52861feae811138ec0077575f636664
Kalyan-Vasanth <156084873+Kalyan-Vasanth@users.noreply.github.com>
2025-01-10 08:08:46 +0530

    Add files via upload

---
+++ week2/maxAscending.cpp
@@ -0,0 +1,69 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define yes          \
+    cout << "YES\n"; \
+    return; 
+#define no          \
+    cout << "NO\n"; \
+    return;
+#define all(arr) (arr).begin(), (arr).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define RREP(i, a, b) for (int i = a; i > b; --i)
+#define debug(x) cout << #x << ": " << x << endl
+#define prt(x) cout << x << '\n'
+#define read(arr)                          \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cin >> (arr)[i];                   \
+    }
+#define print(arr)                         \
+    for (int i = 0; i < (arr).size(); i++) \
+    {                                      \
+        cout << (arr)[i] << " ";           \
+    }                                      \
+    cout << endl;
+#define get(n) \
+    int n;     \
+    cin >> n
+const int N1 = 1e5 + 1;
+const int N2 = 1e7 + 1;
+const int MOD = 1e9 + 7; 
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+int maxAscendingSum(vector<int>& nums) {
+        int  ans = nums[0], sum = nums[0];
+        int n = nums.size();
+        for(int i = 1 ; i<n ; i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                sum+=nums[i];
+            }
+            else
+            {
+                sum = nums[i];
+            }
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    get(t);
+    for (int i = 0; i < t; i++)
+    {
+        get(n);
+        vi arr(n);
+        read(arr);
+        int p = maxAscendingSum(arr);
+        prt(p);
+    }
+    return 0;
+}
